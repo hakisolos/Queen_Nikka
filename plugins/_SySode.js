12242d7b68b263da73f41ebafa725a9917df25fe
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-09-02 10:24:14 +0100

    Fix restart

--- plugins/_SySode.js
+++ plugins/_SySode.js
@@ -31,7 +31,7 @@ smd({
   const {
     exec: _0x1912df
   } = require("child_process");
-  _0x514d3c.reply("Alya is restarting ♻️");
+  _0x514d3c.reply("bot is restarting ♻️");
   _0x1912df("pm2 restart all");
 });
 smd({
