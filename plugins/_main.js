66df46f3a451976e5c5462b253789fc9cfc59bb3
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-09-17 21:41:05 +0100

    Remove repo command

--- plugins/_main.js
+++ plugins/_main.js
@@ -224,26 +224,6 @@ async function getDateTime() {
   };
 }
 smd({
-  pattern: "repo",
-  react: "📁",
-  alias: ["git", "sc", "script"],
-  desc: "Sends info about repo",
-  category: "general",
-  filename: __filename
-}, async _0x45da98 => {
-  try {
-    let {
-      data: _0x44f98c
-    } = await axios.get("https://github.com/hakisolos/Queen_Nikka");
-    let _0x1c73f9 = ("\nQUEEN_NIKKA 👸 ᴀ sɪᴍᴘʟᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ, ᴍᴀᴅᴇ ʙʏ haki ᴀɴᴅ ᴅᴇᴘʟᴏʏᴇᴅ ʙʏ *" + Config.ownername + "*.\n\n  *❲🍀❳ Stars:* " + (_0x44f98c?.stargazers_count || "120+") + " stars\n  *❲🍀❳ Forks:* " + (_0x44f98c?.forks_count || "1000+") + " forks\n  *❲🍀❳ Authors:* Haki and Star King\n  *❲🍀❳ Created On:* " + (_0x44f98c?.created_at || "undefined") + "\n  *❲🍀❳ Repo:* _https://github.com/hakisolos/Queen_Nikka_\n  *❲🍀❳ Scan:* _" + scan + "_" + (Config.caption ? "\n\n" + Config.caption : "")).trim();
-    return await _0x45da98.sendUi(_0x45da98.jid, {
-      caption: _0x1c73f9
-    });
-  } catch (_0x5816fe) {
-    await _0x45da98.error(_0x5816fe + "\n\ncommand: repo", _0x5816fe);
-  }
-});
-smd({
   pattern: "cpu",
   desc: "To check bot status",
   category: "general",
