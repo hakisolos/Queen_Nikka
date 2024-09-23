a09d494b08fce298059390c36d60349830c98651
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-09-23 22:22:56 +0100

    Create aza.js

---
+++ plugins/aza.js
@@ -0,0 +1,28 @@
+smd({
+  pattern: "aza",
+  react: "💳",  // No reaction needed
+  desc: "Displays account information",
+  category: "info",
+  filename: __filename,
+}, async (message) => {
+  try {
+    const azaMessage = `
+￣￣￣￣￣￣￣￣￣￣￣￣￣|
+        6718656033
+         *MONIEPOINT*
+        *EXCEL MAXWELL-UGIAGBE*
+|＿＿＿＿＿＿＿＿＿＿＿＿＿|
+                     \\•◡•)/
+                       \\     / 
+                        ——
+                        |     |
+                        |_   |_
+    `;
+
+    // Use the message.send method directly for the bot to send the message
+    await message.reply(azaMessage);
+
+  } catch (err) {
+    console.error("Error while sending aza message:", err);
+  }
+});
