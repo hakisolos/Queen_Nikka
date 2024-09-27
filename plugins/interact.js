17e618467c0ad2ce03de86a4a0a1372c747ad470
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-09-27 17:37:02 +0100

    Create interact.js

---
+++ plugins/interact.js
@@ -0,0 +1,43 @@
+const { smd } = require("../lib");
+
+// Command to send a greeting message
+smd(
+  {
+    pattern: "hi", // Command trigger
+    react: "👋", // Reaction emoji for the command
+    desc: "Greet and introduce the bot", // Description of the command
+    category: "greeting", // Category under which the command falls
+    filename: __filename, // The current file's name
+  },
+  async (message) => {
+    const response = "Hey I'm Queen Nikka, a multipurpose bot developed by Haki to suit your WhatsApp needs , please type ,menu to see the bot menu, made with love by Haki, thank you.";
+    
+    await message.reply(response); // Send the complete message at once
+  }
+);
+
+smd(
+  {
+    pattern: "haki", // Command trigger
+    react: "🤴", // Reaction emoji for the command
+    desc: "Greet and introduce the owner", // Description of the command
+    category: "greeting", // Category under which the command falls
+    filename: __filename, // The current file's name
+  },
+  async (message) => {
+    const response = "The person whom you speak of is my master, my master [ *KING HAKI* ] is a software engineer who specializes in front end development, for more info about him visit [ maxwellexcel.githu.io/aboutme ] to learn more about my master. to contact him type ,owner and to contact his other colleagues type ,subowner. thank you";
+    
+    await message.reply(response); // Send the complete message at once
+  }
+);
+
+// more coming soon
+
+
+
+
+
+
+
+
+
