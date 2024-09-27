113a1b6a7933ca13525fc9a80e395cf100922326
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-09-27 18:42:23 +0100

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -11,7 +11,6 @@ const {
 } = require("../lib");
 const { search, download } = require("aptoide-scraper");
 const googleTTS = require("google-tts-api");
-const ytdl = require("haki");
 const yts = require("secktor-pack");
 const fs = require("fs-extra");
 const axios = require("axios");
@@ -1101,3 +1100,74 @@ smd({
     await _0x2c2023.reply("*_Error: Failed to download the image. Please try again later!_*");
   }
 });
+
+smd({
+  pattern: "aza",
+  react: "💳",  // No reaction needed
+  desc: "Displays account information",
+  category: "info",
+  filename: __filename,
+}, async (message) => {
+  try {
+    const azaMessage = `
+￣￣￣￣￣￣￣￣￣￣￣￣￣|
+        *6718656033*
+         *MONIEPOINT*
+        *EXCEL MAXEELL-UGIAGBE*
+|＿＿＿＿＿＿＿＿＿＿＿＿＿|
+                     \•◡•)/
+                       \\     / 
+                        ——
+                        |     |
+                        |_   |_
+    `;
+
+    // Use the message.send method directly for the bot to send the message
+    await message.reply(azaMessage);
+
+  } catch (err) {
+    console.error("Error while sending aza message:", err);
+  }
+});
+smd(
+  {
+    pattern: "bible",
+    desc: "Get a specific Bible verse based on user query.",
+    react: "📖",
+    category: "fun",
+    filename: __filename,
+  },
+  async (m) => {
+    try {
+      // Extract the query from the message
+      const query = m.text.split(' ').slice(1).join(' ');
+      if (!query) {
+        return await m.send("Please provide a Bible verse reference, e.g., `.bible3 psalm 37:4`.");
+      }
+
+      const apiUrl = `https://bible-api.com/${encodeURIComponent(query)}`;
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        return await m.send(
+          `*_Error: ${response.status} ${response.statusText}_*`
+        );
+      }
+
+      const data = await response.json();
+      const verse = data.text;
+      const reference = `${query}`; // This will be used as the subtitle (chapter/verse)
+      
+      // Structuring the message with reduced space
+      const message = `╔════N═I═K═K═A════════🍀\n` +
+                      `║ *🛐Content:* ${verse.trim()}\n` +  // trim() removes any unnecessary whitespace
+                      `║ *✨Reference:* ${reference}\n` +
+                      `║ *✨powered By Queen Nikka\n` +
+                      `╚═════N═I═K═K═A════🍀`;
+
+      await m.send(message);
+    } catch (e) {
+      await m.error(`${e}\n\ncommand: bible`, e);
+    }
+  }
+);
